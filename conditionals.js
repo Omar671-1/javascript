37f223ebb97298da97aad119775c4fc5e6c56414
omar ahmed <omarahmed@omars-air.home>
2024-02-27 13:03:22 +0000

    added case swich

---
+++ conditionals.js
@@ -0,0 +1,24 @@
+// if 
+// if else 
+// if else if
+// switch case default 
+// let age = 13; 
+// if (age >= 18) {
+//     console.log("You can watch the movie in the cinema.");
+// } else if (age > 68 && age <= 100) {
+//     console.log("You can watch the movie in the cinema and do not pay.");
+//     } else {
+//         console.log("You are too young to see this film.")
+// }
+
+switch (age) {
+    case 18:
+        console.log("you can watch the movie in cinema, please pay");
+        break;
+    case 60:
+        console.log("you can watch the movie, dont pay")
+        break;
+    default:
+        console.log("sorry you cannot watch the movie you are to young");
+
+}
---
+++ variables.js
@@ -0,0 +1,80 @@
+// var lastName = "Omar"; // ..
+// console.log("My name is " + lastName);
+// console.log("My name is " + lastName);
+// console.log("My name is " + lastName);
+// var age = 20;
+// var degreeTime = 4;
+// console.log(age+ degreeTime);
+
+// console.log("what boys like"); (){
+//     console.log("cars");
+//     console.log("football");
+//     console.log("games");
+//     console.log("boxing");
+// }
+
+// console.log("what girls  like"); (){
+//     console.log("make-up");
+//     console.log("shopping");
+//     console.log("holidays");
+// }
+// boys()
+// function boys() {
+//   console.log("cars");
+//   console.log("football");
+//   console.log("games");
+//   console.log("boxing");
+// }
+
+// function girls(secret, snapchat) {
+//   console.log("make-up");
+//   console.log("bag");
+//   console.log("socialMedia");
+// }
+
+// console.log("what boys like:");
+// boys();
+
+// console.log("what girls like:");
+// girls("bag", "socialMedia");
+// function multiply() {
+//   var x = 20;
+//   var y = 30;
+//   var z = x * y;
+//   return z;
+// }
+// console.log(multiply(10, 30));
+// console.log(multiply(5, 3));
+// console.log(multiply(2, 8));
+// console.log(multiply(6, 9));
+// console.log(multiply(4, 4));
+
+// var salary = 7500;
+// console.log(salary);
+// var tax = 30;
+// var netIncome = salary * tax;
+// console.log(netIncome);
+// var sweets = 23;
+// var sweetsTopUp *8;
+// sweets * = 8;
+// console.log(sweets);
+// var age = 23;
+// var sisterAge = 15;
+// if (age > sisterAge) {
+// if ((age == 18)) console.log(I am 15 years old);
+
+// var age = 50;
+// if (age <= 18 || age <= 20) {
+//   console.log("I am 18 years old");
+// }
+// var age = 50;
+// if (age <= 18 && age => 20) {
+//   console.log("I am 18 years old");
+// }
+// var multiply = function () {
+//   var x = 5;
+//   var y = 3;
+//   var z = x * y;
+//   return z;
+// }
+// console.log((7, 4));
