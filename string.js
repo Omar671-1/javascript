e2447f09e5ae1392231838d81e0ad5d824c50d90
omar ahmed <omarahmed@omars-MacBook-Air.local>
2024-02-27 17:09:58 +0000

    string

---
+++ string.js
@@ -0,0 +1,16 @@
+// let age = 10 // number
+// let isMarried = false // boolean
+// let firstName = "omar12324"// string
+// console.log(firstName.length)
+//   string 
+// let firstName = `Omar ahmed ${age}`; // string template with interpolation
+// let firstName = `omar ahmed(+-0jhjsn${age}`;
+// let firstName = `omar ahmed(+-0jhjsn${age}`;
+// let description = "I live in London";
+// description = description.toUpperCase()
+// console.log(description);
+
+// let fullName = firstName.concat("").concat(lastName);
+// let fullName = firstName + "" + lastName;
+// console.log(fullName.indexOf("l"));
+
